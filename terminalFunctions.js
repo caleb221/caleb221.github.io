ee8ae8397beb974476c53a0395cc0a4c55aaa2af
Caleb <45024239+caleb221@users.noreply.github.com>
2021-06-03 23:41:15 +0800

    Update terminalFunctions.js

--- terminalFunctions.js
+++ terminalFunctions.js
@@ -1,3 +1,5 @@
+
+
 const teacherNav = document.querySelector("#teacherNav");
 const devNav = document.querySelector("#devNav");
 const analyNav = document.querySelector("#analystNav");
@@ -14,36 +16,61 @@ const stackNav = document.querySelector("#stackNav");
 const mastersNav = document.querySelector("#mastersNav");
 const collegeNav = document.querySelector("#collegeNav");
 
-//Mobile version uses modals
-var w =screen.width
+
+var w = screen.width;
+//var textSize = 
+    console.log(document.body.style.font);
 var isMobile = false;
-if( w<=1020)
+if(w<1020)
 {
  isMobile=true;
 }
 
 if(teacherNav!=null){
+    
+    var boxWidth="400px";
+    var boxHeight="400px";
+    if(isMobile)
+        {
+            boxWidth="700px";
+            boxHeight="600px";
+        }
 
-teacherNav.addEventListener('click',()=>{newBox("Teaching","#teacher-content","400px","400px");});
-devNav.addEventListener('click',()=>{newBox("Development","#dev-content","400px","400px");});
-analyNav.addEventListener('click',()=>{newBox("Analysis","#analyst-content","400px","400px");});
-adventureNav.addEventListener('click',()=>{newBox("Adventure","#adventure-content","400px","400px");});
-contactNav.addEventListener('click',()=>{newBox("Contact","#contact-content","400px","400px");
-});
+teacherNav.addEventListener('click',()=>{newBox("Teaching","#teacher-content",boxWidth,boxHeight);});
+devNav.addEventListener('click',()=>{newBox("Development","#dev-content",boxWidth,boxHeight);});
+analyNav.addEventListener('click',()=>{newBox("Analysis","#analyst-content",boxWidth,boxHeight);});
+adventureNav.addEventListener('click',()=>{newBox("Adventure","#adventure-content",boxWidth,boxHeight);});
+contactNav.addEventListener('click',()=>{newBox("Contact","#contact-content",boxWidth,boxHeight);});
+
 }
 else if(espnav!=null)
 {   
-espnav.addEventListener('click',()=>{newBox("ESP32-Leaf","#espLeaf-content","500px","500px");});
- droidnav.addEventListener('click',()=>{newBox("R4-04","#droid-content","500px","500px");});
- turbinenav.addEventListener('click',()=>{newBox("Wind Turbine","#windTurbine-content","500px","500px");}); 
- miscProjectnav.addEventListener('click',()=>{newBox("Other projects","#misc-content","500px","500px");}); 
+    var boxWidth="500px";
+    var boxHeight="500px";
+    if(isMobile)
+        {
+            boxWidth="700px";
+            boxHeight="600px";
+        }   
+ espnav.addEventListener('click',()=>{newBox("ESP32-Leaf","#espLeaf-content",boxWidth,boxHeight);});
+ droidnav.addEventListener('click',()=>{newBox("R4-04","#droid-content",boxWidth,boxHeight);});
+ turbinenav.addEventListener('click',()=>{newBox("Wind Turbine","#windTurbine-content",boxWidth,boxHeight);}); 
+ miscProjectnav.addEventListener('click',()=>{newBox("Other projects","#misc-content",boxWidth,boxHeight);}); 
 }
 else if(expNav!=null)
 {
-    expNav.addEventListener('click',()=>{newBox("Experience","#experience-content","600px","400px");});
-    stackNav.addEventListener('click',()=>{newBox("Stack","#stack-content","400px","400px");}); 
-    mastersNav.addEventListener('click',()=>{newBox("Masters","#masters-content","400px","400px");}); 
-    collegeNav.addEventListener('click',()=>{newBox("Undergraduate","#college-content","400px","400px");}); 
+    var boxWidth="400px";
+    var boxHeight="400px";
+    if(isMobile)
+        {
+            boxWidth="600px";
+            boxHeight="600px";
+        } 
+    
+    expNav.addEventListener('click',()=>{newBox("Experience","#experience-content","600px","500px");});
+    stackNav.addEventListener('click',()=>{newBox("Stack","#stack-content",boxWidth,boxHeight);}); 
+    mastersNav.addEventListener('click',()=>{newBox("Masters","#masters-content",boxWidth,boxHeight);}); 
+    collegeNav.addEventListener('click',()=>{newBox("Undergraduate","#college-content",boxWidth,boxHeight);}); 
 }
 
 else
@@ -70,3 +97,5 @@ function newBox(title,thisId,thisWidth,thisHeight)
             onblur:function(){this.setBackground('#777')}
     }) 
 }
+
+
